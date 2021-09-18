126a05ea8b0811ba829a695537715ebefae0457d
Cédric Krier <ced@b2ck.com>
2021-09-18 14:20:36 +0200

    Increase version number

--- mt940/__init__.py
+++ mt940/__init__.py
@@ -2,7 +2,7 @@
 # this repository contains the full copyright notices and license terms.
 """a parser for MT940 files
 """
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 __all__ = ['MT940', 'rabo_description', 'abn_amro_description',
     'ing_description', 'regiobank_description']
 
